f3b2800d8101b34d9d22ed3d6f6f65afdf26d3a1
SUMUKHA-PK <sumukhapk46@gmail.com>
2018-06-29 23:00:40 +0530

    First blog done

--- Bell/Driver.cs
+++ Bell/Driver.cs
@@ -3,20 +3,20 @@ using Microsoft.Quantum.Simulation.Simulators;
 
 namespace Quantum.Bell  // This is the namespace created in the qs script
 {
-    class Driver
+    class Driver    
     {
         static void Main(string[] args)
         {
-            using (var sim = new QuantumSimulator())
+            using (var sim = new QuantumSimulator()) 
             {
                 // Try initial values
                 Result[] initials = new Result[] { Result.Zero, Result.One };
                 foreach (Result initial in initials)
                 {
                     var res = BellTest.Run(sim, 1000, initial).Result;
-                    var (numZeros, numOnes) = res;
+                    var (numZeros, numOnes,agree) = res;
                     System.Console.WriteLine(
-                        $"Init:{initial,-4} 0s={numZeros,-4} 1s={numOnes,-4}");
+                        $"Init:{initial,-4} 0s={numZeros,-4} 1s={numOnes,-4} agree={agree,-4}");
                 }
             }
             System.Console.WriteLine("Press any key to continue...");
--- Bell/obj/qsharp/src/Bell.g.cs
+++ Bell/obj/qsharp/src/Bell.g.cs
@@ -5,7 +5,7 @@ using Microsoft.Quantum.Simulation.Core;
 using Microsoft.Quantum.MetaData.Attributes;
 
 [assembly: OperationDeclaration("Quantum.Bell", "Set (desired : Result, q1 : Qubit) : ()", new string[] { }, "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs", 302L, 6L, 200L)]
-[assembly: OperationDeclaration("Quantum.Bell", "BellTest (count : Int, initial : Result) : (Int, Int)", new string[] { }, "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs", 550L, 18L, 5L)]
+[assembly: OperationDeclaration("Quantum.Bell", "BellTest (count : Int, initial : Result) : (Int, Int, Int)", new string[] { }, "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs", 556L, 18L, 5L)]
 #line hidden
 namespace Quantum.Bell
 {
@@ -75,7 +75,7 @@ namespace Quantum.Bell
         }
     }
 
-    public class BellTest : Operation<(Int64,Result), (Int64,Int64)>, ICallable
+    public class BellTest : Operation<(Int64,Result), (Int64,Int64,Int64)>, ICallable
     {
         public BellTest(IOperationFactory m) : base(m)
         {
@@ -90,9 +90,9 @@ namespace Quantum.Bell
             System.Collections.Generic.IEnumerable<Qubit> IApplyData.Qubits => null;
         }
 
-        public class Out : QTuple<(Int64,Int64)>, IApplyData
+        public class Out : QTuple<(Int64,Int64,Int64)>, IApplyData
         {
-            public Out((Int64,Int64) data) : base(data)
+            public Out((Int64,Int64,Int64) data) : base(data)
             {
             }
 
@@ -102,6 +102,18 @@ namespace Quantum.Bell
         String ICallable.Name => "BellTest";
         String ICallable.FullName => "Quantum.Bell.BellTest";
         protected Allocate Allocate
+        {
+            get;
+            set;
+        }
+
+        protected IUnitary<(Qubit,Qubit)> MicrosoftQuantumPrimitiveCNOT
+        {
+            get;
+            set;
+        }
+
+        protected IUnitary<Qubit> MicrosoftQuantumPrimitiveH
         {
             get;
             set;
@@ -125,53 +137,71 @@ namespace Quantum.Bell
             set;
         }
 
-        public override Func<(Int64,Result), (Int64,Int64)> Body => (__in) =>
+        public override Func<(Int64,Result), (Int64,Int64,Int64)> Body => (__in) =>
         {
             var (count,initial) = __in;
 #line 21 "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs"
             var numOnes = 0L;
-            // Variables are immutable unless specified like in rust
 #line 22 "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs"
-            var qubits = Allocate.Apply(1L);
-#line 24 "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs"
+            var agree = 0L;
+#line 23 "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs"
+            var qubits = Allocate.Apply(2L);
+#line 25 "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs"
             foreach (var test in new Range(1L, count))
             {
-#line 26 "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs"
+#line 27 "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs"
                 Set.Apply((initial, qubits[0L]));
 #line 28 "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs"
-                var res = M.Apply(qubits[0L]);
-                // Count the number of ones we saw:
+                Set.Apply((Result.Zero, qubits[1L]));
+#line 30 "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs"
+                MicrosoftQuantumPrimitiveH.Apply(qubits[0L]);
 #line 31 "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs"
+                MicrosoftQuantumPrimitiveCNOT.Apply((qubits[0L], qubits[1L]));
+#line 32 "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs"
+                var res = M.Apply(qubits[0L]);
+#line 34 "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs"
+                if ((M.Apply(qubits[1L]) == res))
+                {
+#line 36 "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs"
+                    agree = (agree + 1L);
+                }
+
+                // Count the number of ones we saw:
+#line 40 "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs"
                 if ((res == Result.One))
                 {
-#line 33 "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs"
+#line 42 "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs"
                     numOnes = (numOnes + 1L);
                 }
             }
 
-#line 36 "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs"
+#line 46 "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs"
             Set.Apply((Result.Zero, qubits[0L]));
+#line 47 "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs"
+            Set.Apply((Result.Zero, qubits[1L]));
 #line hidden
             Release.Apply(qubits);
             // Return number of times we saw a |0> and number of times we saw a |1>
-#line 39 "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs"
-            return ((count - numOnes), numOnes);
+#line 50 "/home/some_guyy/pk/git/Quantum-computing/Bell/Bell.qs"
+            return ((count - numOnes), numOnes, agree);
         }
 
         ;
         public override void Init()
         {
             this.Allocate = this.Factory.Get<Allocate>(typeof(Microsoft.Quantum.Primitive.Allocate));
+            this.MicrosoftQuantumPrimitiveCNOT = this.Factory.Get<IUnitary<(Qubit,Qubit)>>(typeof(Microsoft.Quantum.Primitive.CNOT));
+            this.MicrosoftQuantumPrimitiveH = this.Factory.Get<IUnitary<Qubit>>(typeof(Microsoft.Quantum.Primitive.H));
             this.M = this.Factory.Get<ICallable<Qubit, Result>>(typeof(Microsoft.Quantum.Primitive.M));
             this.Release = this.Factory.Get<Release>(typeof(Microsoft.Quantum.Primitive.Release));
             this.Set = this.Factory.Get<ICallable<(Result,Qubit), QVoid>>(typeof(Quantum.Bell.Set));
         }
 
         public override IApplyData __dataIn((Int64,Result) data) => new In(data);
-        public override IApplyData __dataOut((Int64,Int64) data) => new Out(data);
-        public static System.Threading.Tasks.Task<(Int64,Int64)> Run(IOperationFactory __m__, Int64 count, Result initial)
+        public override IApplyData __dataOut((Int64,Int64,Int64) data) => new Out(data);
+        public static System.Threading.Tasks.Task<(Int64,Int64,Int64)> Run(IOperationFactory __m__, Int64 count, Result initial)
         {
-            return __m__.Run<BellTest, (Int64,Result), (Int64,Int64)>((count, initial));
+            return __m__.Run<BellTest, (Int64,Result), (Int64,Int64,Int64)>((count, initial));
         }
     }
 }
